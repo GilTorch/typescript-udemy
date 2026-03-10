e9b0e2aa57031460cd4f15a5a52d07228e87ad55
GilTorch <infotorch2014@gmail.com>
2026-03-10 16:47:35 -0400

    tuples

--- arrays-objects.ts
+++ arrays-objects.ts
@@ -1,3 +1,8 @@
 let hobbies: Array<string | number> = ["Sports", "Cooking"] // <-- generic types
 
 hobbies.push(10);
+ 
+// types
+
+let n: number[] = [1,-1] // <-- works but not precise
+let x: [number, number] = [1,-1] // <-- even better
