c693e8845a50f6a032237ef86bf542f8e301ace9
GilTorch <infotorch2014@gmail.com>
2026-03-10 16:19:23 -0400

    assign types to function params

--- basics.ts
+++ basics.ts
@@ -1,2 +1,7 @@
 let userName: string;  // you want to set a type
 let userAge = 38; // typescript can infer type
+
+
+function add(a: number, b: number){ // you can assign types to function params
+    return a + b;
+}
