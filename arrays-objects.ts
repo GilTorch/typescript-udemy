e3234bff2101def2981581e744aadfef2f1d99d7
GilTorch <infotorch2014@gmail.com>
2026-03-10 16:57:38 -0400

    type {}

--- arrays-objects.ts
+++ arrays-objects.ts
@@ -6,3 +6,9 @@ hobbies.push(10);
 
 let n: number[] = [1,-1] // <-- works but not precise
 let x: [number, number] = [1,-1] // <-- even better
+
+// type {} means any value that is not undefined or. null
+
+let c: {} = 3 // <- works
+let d: {} = null // <- ❌
+let e: {} = undefined // <- ❌
